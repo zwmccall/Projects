9989392c1f8568de308538572c71e234e4f610ff
zwmccall <32351629+zwmccall@users.noreply.github.com>
2022-02-02 13:34:00 -0600

    Made inactive rows look different, fixed input to accept per row and change to new row when enter is pressed on a full row

--- wordle-test/src/index.js
+++ wordle-test/src/index.js
@@ -2,23 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const numOfFields = 4;
+const numOfFields = 5;
 
 const handleChange = e => {
   const { maxLength, value, name } = e.target;
   const [fieldName, fieldIndex] = name.split("-");
+  const [rowName, rowIndex] = e.target.parentElement.getAttribute("name").split("-");
 
   // Check if they hit the max character length
   if (value.length >= maxLength) {
     // Check if it's not the last input field
-    if (parseInt(fieldIndex, 10) < numOfFields) {
+    //console.log(parseInt(fieldIndex-1, 10));
+    //console.log(numOfFields * rowIndex);
+    if (parseInt(fieldIndex, 10) < (numOfFields * rowIndex) - 1) {
       // Get the next input field
+      console.log("here");
       const nextSibling = document.querySelector(
         `input[name=item-${parseInt(fieldIndex, 10) + 1}]`
       );
+      //console.log(nextSibling);
 
       // If found, focus the next field
       if (nextSibling !== null) {
+        //console.log("trying");
         nextSibling.focus();
       }
     } else {
@@ -33,31 +39,29 @@ const handleChange = e => {
         }
     }
   } else {
-    if(e.key === 'Enter'){
-        console.log('init');
-        for(var i = 5; i < 10; i++){
-            const nextSibling = document.querySelector(
-                `input[name=item-${i}]`
-            );
-            nextSibling.classList.remove("inactive-square");
+    if(e.key === 'Backspace'){
+        if (parseInt(fieldIndex, 10) > 0) {
+          console.log('deleting');
+          // Get the next input field
+          const prevSibling = document.querySelector(
+            `input[name=item-${parseInt(fieldIndex, 10) - 1}]`
+          );
+    
+          // If found, focus the next field
+          if (prevSibling !== null) {
+            prevSibling.focus();
+          }
+        } else {
+          console.log('first');
         }
     }
   }
 }
 
-function initSquares(){
-    for(var i = 5; i < 10; i++){
-        const nextSibling = document.querySelector(
-            `input[name=item-${i}]`
-        );
-        nextSibling.classList.add("inactive-square");
-    }
-}
-
 class Square extends React.Component {
     render() {
       return (
-        <input maxLength="1" onKeyDown={handleChange} name={`item-${ this.props.value }`} className={`square ${if(this.props.value >= 5){"inactive-square"}}}`}>
+        <input maxLength="1" onKeyDown={handleChange} name={`item-${ this.props.value }`} className={`square ${this.props.value > 4 ? "inactive-square" : ""}`}>
           {/* TODO */}
         </input>
       );
@@ -70,26 +74,26 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Welcome to Wordle Clone';
       
       return (
         <div>
           <div className="status">{status}</div>
-          <div className="board-row">
+          <div className="board-row" name="row-1">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
             {this.renderSquare(3)}
             {this.renderSquare(4)}
           </div>
-          <div className="board-row">
+          <div className="board-row" name="row-2">
             {this.renderSquare(5)}
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
             {this.renderSquare(9)}
           </div>
-          <div className="board-row">
+          <div className="board-row" name="row-3">
             {this.renderSquare(10)}
             {this.renderSquare(11)}
             {this.renderSquare(12)}
